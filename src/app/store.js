ccd7c7301dc5069c7a833548c27cfac2f974ade7
Riccardo Silvi <r.silvi@young.business>
2022-07-06 18:03:09 +0200

    store configuration with apiSlice reducer and middlware

--- src/app/store.js
+++ src/app/store.js
@@ -4,10 +4,19 @@ import postsReducer from '../features/posts/postsSlice'
 import usersReducer from '../features/users/usersSlice'
 import notificationsReducer from '../features/notifications/notificationsSlice'
 
+import { apiSlice } from '../features/api/apiSlice'
+
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
     notifications: notificationsReducer,
+    [apiSlice.reducerPath] : apiSlice.reducer
   },
+  // thunk middleware is "on by default"
+  // we need to add the apiSlice middleware
+  // it manages cache lifetimes and expiration
+  // it should be placed after thunk and other middlewares
+  middleware : getDefaultMiddleware =>
+      getDefaultMiddleware().concat(apiSlice.middleware)
 })
