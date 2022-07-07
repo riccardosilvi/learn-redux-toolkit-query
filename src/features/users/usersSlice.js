27532e93f146b5394f80baceadcccf6b8920413b
Riccardo Silvi <r.silvi@young.business>
2022-07-07 12:47:43 +0200

    usersSlice refactor to select from getUsers query and create derived selectors

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,31 +1,37 @@
 import {
-  createSlice,
-  createAsyncThunk,
-  createEntityAdapter
+  createSelector
 } from "@reduxjs/toolkit";
-import { client } from "../../api/client";
-
+import { apiSlice } from "../api/apiSlice";
+/*
 const usersAdapter = createEntityAdapter();
 
 const initialState = usersAdapter.getInitialState();
 
-export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
-  const response = await client.get("/fakeApi/users");
-  return response.data;
-});
+ */
 
-const usersSlice = createSlice({
-  name: "users",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(fetchUsers.fulfilled, usersAdapter.setAll);
-  }
-});
+//since we invoke the getUsers query in index js via the initiate function
+//we can select its fetched data through the endpoints[queryName].select method
 
-export default usersSlice.reducer;
+export const selectUserResults = apiSlice.endpoints.getUsers.select()
 
+//we can then build upon that with reselect composed and memoized selectors
+
+const emptyUsers = []
+
+export const selectAllUsers = createSelector(
+  selectUserResults,
+  userResults => userResults?.data ?? emptyUsers //provide a fallback data set
+)
+
+export const selectUserById = createSelector(
+    selectAllUsers,
+    (state, userId) => userId,
+    (users, userId) => users.find(user => user.id === userId)
+)
+
+/*
 export const {
   selectAll: selectAllUsers,
   selectById: selectUserById
 } = usersAdapter.getSelectors((state) => state.users);
+*/
