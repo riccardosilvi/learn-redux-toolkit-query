c0c34b7d98e4216f422c72f551bb9ca4f43305db
Riccardo Silvi <r.silvi@young.business>
2022-07-07 10:28:53 +0200

    SinglePostPage refactor with useGetPostQuery hook

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,39 +1,41 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { PostAuthor } from './PostAuthor'
 import { TimeAgo } from './TimeAgo'
 import { ReactionButtons } from './ReactionButtons'
-import { selectPostById } from './postsSlice'
+import { useGetPostQuery } from "../api/apiSlice";
+import { Spinner } from "../../components/Spinner";
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) => selectPostById(state, postId))
+  const {
+    data: post,
+    isFetching,//each loading attempt boolean flag
+    isSuccess //first post request attempt success boolean flag(do we have cached data?)
+  } = useGetPostQuery(postId)
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
+  let content
+
+  if(isFetching){
+    content = <Spinner text="Loading..." />
+  } else if(isSuccess){
+    content = (
+        <article className="post">
+          <h2>{post.title}</h2>
+          <div>
+            <PostAuthor userId={post.user} />
+            <TimeAgo timestamp={post.date} />
+          </div>
+          <p className="post-content">{post.content}</p>
+          <ReactionButtons post={post} />
+          <Link to={`/editPost/${post.id}`} className="button">
+            Edit Post
+          </Link>
+        </article>
     )
   }
 
-  return (
-    <section>
-      <article className="post">
-        <h2>{post.title}</h2>
-        <div>
-          <PostAuthor userId={post.user} />
-          <TimeAgo timestamp={post.date} />
-        </div>
-        <p className="post-content">{post.content}</p>
-        <ReactionButtons post={post} />
-        <Link to={`/editPost/${post.id}`} className="button">
-          Edit Post
-        </Link>
-      </article>
-    </section>
-  )
+  return <section>{content}</section>
 }
