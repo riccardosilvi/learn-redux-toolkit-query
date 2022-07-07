03ed08cdb873cc97364be178b76396b2355a6165
Riccardo Silvi <r.silvi@young.business>
2022-07-07 11:36:54 +0200

    EditPostForm refactor with apiSlice useGetPostQuery and useEditPostMutation hooks

--- src/features/posts/EditPostForm.js
+++ src/features/posts/EditPostForm.js
@@ -1,54 +1,69 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
-import { postUpdated, selectPostById } from './postsSlice'
+import { useEditPostMutation, useGetPostQuery } from "../api/apiSlice";
+import { Spinner } from "../../components/Spinner";
 
 export const EditPostForm = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector((state) => selectPostById(state, postId))
+  const {
+      data : post,
+      isSuccess, //we have cached data
+  } = useGetPostQuery(postId)
 
   const [title, setTitle] = useState(post.title)
   const [content, setContent] = useState(post.content)
 
-  const dispatch = useDispatch()
+  const [editPost, { isLoading }] = useEditPostMutation()
   const history = useHistory()
 
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
 
-  const onSavePostClicked = () => {
+  const onSavePostClicked = async () => {
     if (title && content) {
-      dispatch(postUpdated({ id: postId, title, content }))
+      await editPost({ id: postId, title, content })
       history.push(`/posts/${postId}`)
     }
   }
 
+  let renderedContent
+
+  if(isLoading){
+      renderedContent = <Spinner text={"Loading..."} />
+  } else if (isSuccess){
+      renderedContent = (
+        <>
+            <h2>Edit Post</h2>
+            <form>
+                <label htmlFor="postTitle">Post Title:</label>
+                <input
+                    type="text"
+                    id="postTitle"
+                    name="postTitle"
+                    placeholder="What's on your mind?"
+                    value={title}
+                    onChange={onTitleChanged}
+                />
+                <label htmlFor="postContent">Content:</label>
+                <textarea
+                    id="postContent"
+                    name="postContent"
+                    value={content}
+                    onChange={onContentChanged}
+                />
+            </form>
+            <button type="button" onClick={onSavePostClicked}>
+                Save Post
+            </button>
+        </>
+      )
+  }
+
   return (
     <section>
-      <h2>Edit Post</h2>
-      <form>
-        <label htmlFor="postTitle">Post Title:</label>
-        <input
-          type="text"
-          id="postTitle"
-          name="postTitle"
-          placeholder="What's on your mind?"
-          value={title}
-          onChange={onTitleChanged}
-        />
-        <label htmlFor="postContent">Content:</label>
-        <textarea
-          id="postContent"
-          name="postContent"
-          value={content}
-          onChange={onContentChanged}
-        />
-      </form>
-      <button type="button" onClick={onSavePostClicked}>
-        Save Post
-      </button>
+        {renderedContent}
     </section>
   )
 }
