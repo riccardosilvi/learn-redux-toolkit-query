223e849e1e42f1c0d44ec9f535cf326e34cb4e98
Riccardo Silvi <r.silvi@young.business>
2022-07-07 12:02:19 +0200

    apiSlice posts list and by item id tag provide/invalidate logic

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -11,10 +11,19 @@ export const apiSlice = createApi({
     endpoints: builder => ({
         getPosts: builder.query({
             query:  () => '/posts', // '/fakeApi/posts'
-            providesTags : ['Post'] // data the query provides by tags
+            // data the query provides by tags
+            // as a callback we can receive the query response (result)
+            // and compute a series of tags
+            providesTags : (result= []) => [
+                { type: 'Post', id : 'LIST' }, // full list
+                ...result.map(({id}) => ({ type: 'Post', id })) // from posts ids
+            ]
         }),
         getPost : builder.query({
-            query: (postId) => `/posts/${postId}`// '/fakeApi/posts/{postId}'
+            query: (postId) => `/posts/${postId}`,// '/fakeApi/posts/{postId}'
+            // a tag is provided based on post id
+            // arg is the query argument ( postId )
+            providesTags : (_,__, arg) => [{ type: 'Post', id: arg }]
         }),
         addNewPost : builder.mutation({
             query : (updatedPost) => ({
@@ -22,14 +31,20 @@ export const apiSlice = createApi({
                 url : '/posts', // '/fakeApi/posts'
                 body: updatedPost
             }),
-            invalidatesTags : ['Post'] // data types which cache gets outdated on mutation usage as strings
+            // data types which cache gets outdated on mutation usage as strings
+            // only the list will be refetched
+            invalidatesTags : [{ type: 'Post', id : 'LIST' }]
         }),
         editPost : builder.mutation({
             query: post => ({
                 method: "PATCH",
                 url: `/posts/${post.id}`,// '/fakeApi/posts'
                 body: post
-            })
+            }),
+            // invalidates based on post id
+            // arg is the query argument ( post )
+            // LIST tag gets automatically invalidated
+            invalidatesTags : (_, __, arg) => [{ type: 'Post', id : arg.id}]
         })
     })
 })
